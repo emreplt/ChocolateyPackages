a021bb94054c1587773939c87f0a000242f90ac8
Jakub Bereżański <kuba@berezanscy.pl>
2019-04-19 22:57:55 +0200

    fixup! refactor .NET packages, add netfx-4.6.2 and netfx-4.8

--- netfx-4.8/tools/ChocolateyInstall.ps1
+++ netfx-4.8/tools/ChocolateyInstall.ps1
@@ -6,8 +6,8 @@ $arguments = @{
     Release = 528040
     Version = $version
     ProductNameWithVersion = "Microsoft .NET Framework $version"
-    $Url = 'https://download.visualstudio.microsoft.com/download/pr/7afca223-55d2-470a-8edc-6a1739ae3252/abd170b4b0ec15ad0222a809b761a036/NDP48-x86-x64-AllOS-ENU.exe'
-    $Checksum = '95889D6DE3F2070C07790AD6CF2000D33D9A1BDFC6A381725AB82AB1C314FD53'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/7afca223-55d2-470a-8edc-6a1739ae3252/abd170b4b0ec15ad0222a809b761a036/NDP48-x86-x64-AllOS-ENU.exe'
+    Checksum = '95889D6DE3F2070C07790AD6CF2000D33D9A1BDFC6A381725AB82AB1C314FD53'
     ChecksumType = 'sha256'
 }
 
